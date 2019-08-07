6c65daf8b1f34a6122472322b793512603e014d9
Brandon Fields <brandonfields10@icloud.com>
2019-08-07 18:58:42 -0400

    redoing work, had the wrong key

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # liri-node-app
-LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+
+In this assignment, you will make LIRI. LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+# Deployment
+Clone repo
+Run npm install
+At command prompt run node liri.js <pass in an instruction from above>
 
 
-To retrieve the data that will power this app, you'll need to send requests using the `axios` package to the Bands in Town, Spotify and OMDB APIs. 
+# Tech used
+Node.js
+* [Node-Spotify-API](https://www.npmjs.com/package/node-spotify-api)
+* [Axios](https://www.npmjs.com/package/axios)
+* You'll use Axios to grab data from the [OMDB API](http://www.omdbapi.com) and the [Bands In   Town API](http://www.artists.bandsintown.com/bandsintown-api)
+* [Moment](https://www.npmjs.com/package/moment)
+* [DotEnv](https://www.npmjs.com/package/dotenv)
+
+
+# Screenshots - 
+
+
+
+
+# Author 
+Brandon Fields
---
+++ liri.js
@@ -0,0 +1,113 @@
+require("dotenv").config();
+
+var axios = require("axios");
+var keys = require("../Liri-Bot/keys");
+var fs = require('fs');
+var spotify = require('node-spotify-api');
+
+var Spotify = new Spotify(keys.spotify);
+
+var userCommand = process.argv[2];
+
+if (userCommand === "concert-this") {
+    var artist = process.argv[3];
+    concertthis();
+} else if (command === "spotify-this") {
+    var songTitle = process.argv[3];
+    spotifyThis();
+} else if (command === "movie-this") {
+    var movie = process.argv[3];
+    moviethis();
+} else if (command === "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+
+        var dataArr = data.split(",");
+        var command = dataArr[0];
+
+        if (command === "concert-this") {
+            artist = dataArr[1];
+            console.log(dataArr[1])
+            concertthis();
+        } else if (command === "spotify-this-song") {
+            songTitle = dataArr[1];
+            spotifyThis();
+        } else if (command === "movie-this") {
+            movie = dataArr[1];
+            moviethis();
+        }
+    })
+}
+
+
+
+
+function moviethis() {
+    if (!movie) {
+        axios.get("https://www.omdbapi.com/?t=Mr.Nobody&apikey=trilogy").then(
+            function (response) {
+                console.log((response.data.Title).toUpperCase())
+                console.log(response.data.Year);
+                console.log("IMDB: " + response.data.imdbRating);
+                console.log(response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value);
+                console.log("Produced in " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log(response.data.Actors);
+
+            })
+
+    } else {
+        axios.get("https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy").then(
+            function (reponse){
+                console.log((response.data.Title).toUpperCase())
+                console.log(response.data.Year);
+                console.log("IMDB: " + response.data.imdbRating);
+                console.log(response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value);
+                console.log("Produced in " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log(response.data.Actors);
+            }
+        )
+
+    }
+
+    function spotifyThis() {
+        spotify.search({ type: 'track', query: songTitle }, function (err, data) {
+            if (error) {
+                return console.log('Error occured: ' + error);
+            }
+            for (i = 0; i < data.tracks.items.length; i++) {
+                fs.appendFile("sample.txt", JSON.stringify(
+                    console.log("***" + (data.tracks.items[i].name).toUpperCase() + "*****"),
+                    console.log("ARTIST: " + data.tracks.items[i].artists[0].name),
+                    console.log("ALBUM: " + data.tracks.items[i].album.name),
+                    console.log(data.tracks.items[i].external_urls.spotify),
+                    console.log("")), function (err) {
+                    });
+                };
+            });
+        }
+    }
+
+
+function concertthis(){
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            console.log("--------")
+            console.log(artist.toUpperCase() + " has shows at these arena: ")
+            console.log("--------")
+            for(i = 0; i < response.data.length; i++){
+                console.log(response.data[i].venue.name);
+                console.log(response.data[i].venue.city + "," + response.data[i].venue.Country)
+                console.log(response.data[i].datetime);
+                console.log("=========");
+            }
+        }
+    )
+} 
+
+
