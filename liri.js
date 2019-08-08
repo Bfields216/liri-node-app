a60793a2519ddd8fe298b8372ca66a9c0d152e54
Brandon Fields <brandonfields10@icloud.com>
2019-08-08 18:31:14 -0400

    updated code more got to finish it

--- liri.js
+++ liri.js
@@ -1,24 +1,24 @@
 require("dotenv").config();
 
 var axios = require("axios");
-var keys = require("../Liri-Bot/keys");
+var keys = require("./keys");
 var fs = require('fs');
-var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 
-var Spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 var userCommand = process.argv[2];
 
 if (userCommand === "concert-this") {
     var artist = process.argv[3];
     concertthis();
-} else if (command === "spotify-this") {
+} else if (userCommand === "spotify-this") {
     var songTitle = process.argv[3];
     spotifyThis();
-} else if (command === "movie-this") {
+} else if (userCommand === "movie-this") {
     var movie = process.argv[3];
     moviethis();
-} else if (command === "do-what-it-says") {
+} else if (userCommand === "do-what-it-says") {
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             return console.log(error);
@@ -52,7 +52,7 @@ function moviethis() {
                 console.log(response.data.Year);
                 console.log("IMDB: " + response.data.imdbRating);
                 console.log(response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value);
-                console.log("Produced in " + response.data.Country);
+                console.log("Made in " + response.data.Country);
                 console.log("Language: " + response.data.Language);
                 console.log("Plot: " + response.data.Plot);
                 console.log(response.data.Actors);
@@ -61,7 +61,7 @@ function moviethis() {
 
     } else {
         axios.get("https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy").then(
-            function (reponse){
+            function (reponse) {
                 console.log((response.data.Title).toUpperCase())
                 console.log(response.data.Year);
                 console.log("IMDB: " + response.data.imdbRating);
@@ -75,32 +75,16 @@ function moviethis() {
 
     }
 
-    function spotifyThis() {
-        spotify.search({ type: 'track', query: songTitle }, function (err, data) {
-            if (error) {
-                return console.log('Error occured: ' + error);
-            }
-            for (i = 0; i < data.tracks.items.length; i++) {
-                fs.appendFile("sample.txt", JSON.stringify(
-                    console.log("***" + (data.tracks.items[i].name).toUpperCase() + "*****"),
-                    console.log("ARTIST: " + data.tracks.items[i].artists[0].name),
-                    console.log("ALBUM: " + data.tracks.items[i].album.name),
-                    console.log(data.tracks.items[i].external_urls.spotify),
-                    console.log("")), function (err) {
-                    });
-                };
-            });
-        }
-    }
+}
 
 
-function concertthis(){
+function concertthis() {
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
         function (response) {
             console.log("--------")
             console.log(artist.toUpperCase() + " has shows at these arena: ")
             console.log("--------")
-            for(i = 0; i < response.data.length; i++){
+            for (i = 0; i < response.data.length; i++) {
                 console.log(response.data[i].venue.name);
                 console.log(response.data[i].venue.city + "," + response.data[i].venue.Country)
                 console.log(response.data[i].datetime);
@@ -108,6 +92,21 @@ function concertthis(){
             }
         }
     )
-} 
+}
 
-
+function spotifyThis() {
+    spotify.search({ type: 'track', query: songTitle }, function (error, data) {
+        if (error) {
+            return console.log('Error occured: ' + error);
+        }
+        for (i = 0; i < data.tracks.items.length; i++) {
+            fs.appendFile("sample.txt", JSON.stringify(
+                console.log("*" + (data.tracks.items[i].name).toUpperCase() + "*"),
+                console.log("ARTIST: " + data.tracks.items[i].artists[0].name),
+                console.log("ALBUM: " + data.tracks.items[i].album.name),
+                console.log(data.tracks.items[i].external_urls.spotify),
+                console.log("")), function (error) {
+                });
+        };
+    });
+}
